53b85e9ef2bc64f3b995d457692763f9012b1574
Aeryn <59849042+burgeaer@users.noreply.github.com>
2020-01-20 18:11:56 -0600

    Revert "Burgeaer java script elements"

--- Chapter10/Chapter.10/Chapter/10.3/inc_OnlineStoreDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$ErrorMsgs = array();
-$DBConnect =  @mysqli_connect("localhost", "aburgess", "VaniilaLatte0824",
-"aburgess_online_stores");
-if (mysqli_errno($DBConnect)) {
-	$ErrorMsgs[] = "Unable to connect to the database server." . " Error code " . mysqli_errno($DBConnect) . ": " . mysqli_error($DBConnect);
-} 
-?>
--- Chapter10/Chapter.10/Chapter/10.7-10.8/class_OnlineStore.php
+++
@@ -1,8 +0,0 @@
-<?php
-class OnlineStore {
-     private $DBConnect = NULL;
-     private $storeID = "";
-     private $inventory = array();
-     private $shoppingCart = array();
-}
-?>
